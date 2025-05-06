c448fa3518b23c23b0869519c3179e7928997743
Matt Peterson <matt.peterson@tempworks.com>
2025-05-06 17:04:50 -0500

    laid out plans for MagicDeck functionality

--- Controllers/MagicDeckController.cs
+++ Controllers/MagicDeckController.cs
@@ -2,5 +2,22 @@
 
 public class MagicDeckController
 {
-
+    /*
+     * 
+     * TODO:
+     * 
+     * - Create endpoint
+     *   - Creates a MagicDeck record
+     *   - Create request object for creating a MagicDeck. Ensure Cards is nullable.
+     * 
+     * - Update deck Endpoint
+     *   - Allow updates to name, description, format, and commander
+     * 
+     * - Delete deck endpoint
+     * 
+     * - Add Card(s) to deck
+     * 
+     * - Remove Card(s) from deck
+     * 
+     * */
 }
--- Models/Decks/MagicDeck.cs
+++ Models/Decks/MagicDeck.cs
@@ -3,8 +3,10 @@
 public class MagicDeck
 {
     public required Guid Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public DeckFormat Format { get; set; }
-    public List<MagicCard> Cards { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required DeckFormat Format { get; set; }
+    public required List<MagicCard> Cards { get; set; }//Should this be a list of card objects or just GUID's?
+    public MagicCard? Commander { get; set; }
+    public bool FullyObtained { get; set; }
 }
