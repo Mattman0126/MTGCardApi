3b220a14585230eb42acef2bb67aa10c0059e0d6
Matt Peterson <matt.peterson@tempworks.com>
2025-05-07 09:16:30 -0500

    defined DeckCard object

--- Models/Decks/MagicDeck.cs
+++ Models/Decks/MagicDeck.cs
@@ -6,7 +6,16 @@ public class MagicDeck
     public required string Name { get; set; }
     public required string Description { get; set; }
     public required DeckFormat Format { get; set; }
-    public required List<MagicCard> Cards { get; set; }//Should this be a list of card objects or just GUID's?
+    public List<DeckCard> Cards { get; set; } = new();
     public MagicCard? Commander { get; set; }
     public bool FullyObtained { get; set; }
+}
+public class DeckCard
+{
+    public required Guid DeckId { get; set; }
+    public required MagicDeck Deck { get; set; }
+    public required Guid CardId { get; set; }
+    public required MagicCard Card { get; set; } //navigation property
+    public required int Quantity { get; set; }
+    public bool Obtained { get; set; }
 }
